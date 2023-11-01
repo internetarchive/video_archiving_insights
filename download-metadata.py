f6f50aaaf0e7c82eb13e63fb0f9fc88be48f48a7
Will Howes <willmhowes@gmail.com>
2023-11-01 16:33:44 -0700

    put all logic into a main function

--- download-metadata.py
+++ download-metadata.py
@@ -9,7 +9,7 @@ from dotenv import dotenv_values
 from internetarchive import get_session
 
 THREAD_COUNT = 24
-DATE = (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
+DEFAULT_DATE = (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")
 EXCLUDECOLS = [
     "upload_date",
     "channel_id",
@@ -26,47 +26,50 @@ def download_item(identifier, name):
     identifier.download(name)
 
 
-config = dotenv_values(".env")
+def main(date=DEFAULT_DATE):
+    config = dotenv_values(".env")
 
-cookies = {
-    "cookies": {
-        "logged-in-user": config["logged-in-user"],
-        "logged-in-sig": config["logged-in-sig"],
+    cookies = {
+        "cookies": {
+            "logged-in-user": config["logged-in-user"],
+            "logged-in-sig": config["logged-in-sig"],
+        }
     }
-}
-s = get_session(config=cookies)
-item = s.get_item((f"YT-VIDEO-METADATA-{DATE}"))
-files = [(item, f"YT-VIDEO-METADATA-{DATE}-{i:02}.jsonl.gz") for i in range(0, 24)]
-with ThreadPool(THREAD_COUNT) as pool:
-    pool.starmap(download_item, files)
+    s = get_session(config=cookies)
+    item = s.get_item((f"YT-VIDEO-METADATA-{date}"))
+    files = [(item, f"YT-VIDEO-METADATA-{date}-{i:02}.jsonl.gz") for i in range(0, 24)]
+    with ThreadPool(THREAD_COUNT) as pool:
+        pool.starmap(download_item, files)
 
-for i in range(0, 24):
-    with gzip.open(
-        f"YT-VIDEO-METADATA-{DATE}/YT-VIDEO-METADATA-{DATE}-{i:02}.jsonl.gz",
-        "rb",
-    ) as f_in:
-        with open(
-            f"YT-VIDEO-METADATA-{DATE}/YT-VIDEO-METADATA-{DATE}-{i:02}.jsonl",
-            "wb",
-        ) as f_out:
-            shutil.copyfileobj(f_in, f_out)
+    for i in range(0, 24):
+        with gzip.open(
+            f"YT-VIDEO-METADATA-{date}/YT-VIDEO-METADATA-{date}-{i:02}.jsonl.gz",
+            "rb",
+        ) as f_in:
+            with open(
+                f"YT-VIDEO-METADATA-{date}/YT-VIDEO-METADATA-{date}-{i:02}.jsonl",
+                "wb",
+            ) as f_out:
+                shutil.copyfileobj(f_in, f_out)
 
-output_jsonl = []
+    if os.path.isfile(f"video-metadata-with-lang-{date}.jsonl"):
+        os.remove(f"video-metadata-with-lang-{date}.jsonl")
 
-if os.path.isfile(f"video-metadata-with-lang-{DATE}.jsonl"):
-    os.remove(f"video-metadata-with-lang-{DATE}.jsonl")
-
-for i in range(0, 24):
-    with jsonlines.open(
-        f"YT-VIDEO-METADATA-{DATE}/YT-VIDEO-METADATA-{DATE}-{i:02}.jsonl"
-    ) as reader:
+    for i in range(0, 24):
         with jsonlines.open(
-            f"video-metadata-with-lang-{DATE}.jsonl", mode="a"
-        ) as writer:
-            lines = copy.deepcopy(list(reader.iter()))
-            for line in lines:
-                for col in EXCLUDECOLS:
-                    del line[col]
-                writer.write(line)
+            f"YT-VIDEO-METADATA-{date}/YT-VIDEO-METADATA-{date}-{i:02}.jsonl"
+        ) as reader:
+            with jsonlines.open(
+                f"video-metadata-with-lang-{date}.jsonl", mode="a"
+            ) as writer:
+                lines = copy.deepcopy(list(reader.iter()))
+                for line in lines:
+                    for col in EXCLUDECOLS:
+                        del line[col]
+                    writer.write(line)
 
-shutil.rmtree(f"YT-VIDEO-METADATA-{DATE}")
+    shutil.rmtree(f"YT-VIDEO-METADATA-{date}")
+
+
+if __name__ == "__main__":
+    main()
